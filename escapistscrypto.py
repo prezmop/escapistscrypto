35a25935b1b9e3e7100621798301c75e00895721
prezmop <60825986+prezmop@users.noreply.github.com>
2021-08-17 06:00:41 +0200

    moved license to the end of the file

--- escapistscrypto.py
+++ escapistscrypto.py
@@ -2,19 +2,7 @@
 
 # escapistscrypto
 # Copyright © 2021 Przemysław Piasecki
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# check the end of this file for license information
 
 from pathlib import Path
 import argparse
@@ -103,3 +91,15 @@ def cli():
 if __name__ == "__main__":
 	cli()
 	
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
